e6191a45823861c6dbb2363ba8bb62e3655a608e
vicktor349 <ayomidevictor349@gmail.com>
2023-04-30 17:38:59 +0100

    Made changes

--- src/components/NewTask.js
+++ src/components/NewTask.js
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Input } from '@mantine/core'
 import { IoIosAddCircleOutline } from 'react-icons/io'
+import { CgAddR } from 'react-icons/cg'
+import axios from 'axios'
+
 
 
 const NewTask = () => {
+    const [isShown, setIsShown] = useState(false)
+    const [task, setTask] = useState('')
+    const postData = () => {
+        axios.post(`https://644c6a5057f12a1d3dcabf3e.mockapi.io/data`, {
+            task
+        })
+    }
+    const handleClick = () => {
+        setIsShown(current => !current)
+    }
+
+
     return (
-        <div className='text-[#B47AEA] text-xl font-bold hover:underline cursor-pointer flex items-center w-fit mt-24'>
-            <IoIosAddCircleOutline className='text-2xl' />
-            <h1 className='ml-3'>Add New Task</h1>
-        </div>
+        <>
+            <div onClick={handleClick} className='text-[#B47AEA] text-xl font-bold hover:underline cursor-pointer flex items-center w-fit mt-24'>
+                <IoIosAddCircleOutline className='text-2xl' />
+                <h1 className='ml-3'>Add New Task</h1>
+            </div>
+            {isShown ? (
+                <div className='mt-5'>
+                    <Input
+                        className='w-'
+                        name='task'
+                        onChange={(e) => setTask(e.target.value)}
+                        placeholder='Enter A New Task'
+                        rightSection={
+                            <CgAddR onClick={postData} type='submit' className='cursor-pointer text-[#B47AEA]' />
+                        }
+                    />
+                </div>
+            ) : null}
+        </>
     )
 }
 
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,30 +1,53 @@
+import React, { useEffect, useState } from 'react'
 import { Checkbox } from '@mantine/core'
-import React from 'react'
 import { FiTrash2 } from 'react-icons/fi'
+import { BiEditAlt } from 'react-icons/bi'
+import axios from 'axios'
+
 
 const Task = () => {
+    const [apiData, setApiData] = useState([])
+
+    // API REQUEST FOR FETCHING DATA
+    useEffect(() => {
+        axios.get(`https://644c6a5057f12a1d3dcabf3e.mockapi.io/data`)
+            .then((response) => {
+                setApiData(response.data)
+            })
+    }, [])
+
+    const displayData = (id) => {
+        const finalData = apiData.filter((result) => result.id === id)
+        setApiData(finalData)
+    }
+    console.log(displayData)
+
+    // API REQUEST FOR DELETING DATA
+    const handleDelete = async (id) => {
+        try {
+            await axios({
+                method: 'DELETE',
+                url: `https://644c6a5057f12a1d3dcabf3e.mockapi.io/data/${id}`,
+                headers: { 'Content-Type': 'application/json' }
+            })
+            const results = apiData.filter((result) => result.id !== id)
+            setApiData(results)
+        } catch (err) {
+            console.log(err)
+        }
+    }
     return (
-        <div className='text-lg mt-6'>
-            <div className='w-fit flex items-center'>
-                <FiTrash2 className='text-xl' />
-                <Checkbox className='p-4' />
-                <p >Morning Walk</p>
-            </div>
-            <div className='w-fit flex items-center'>
-                <FiTrash2 className='text-xl' />
-                <Checkbox className='p-4' />
-                <p >Lunch</p>
-            </div>
-            <div className='w-fit flex items-center'>
-                <FiTrash2 className='text-xl' />
-                <Checkbox className='p-4' />
-                <p >Dinner with Shobit</p>
-            </div>
-            <div className='w-fit flex items-center'>
-                <FiTrash2 className='text-xl' />
-                <Checkbox className='p-4' />
-                <p >Night Reading</p>
-            </div>
+        <div className='mt-6'>
+            {apiData.map((data, id) => (
+                <div key={id} className='text-lg mt-5'>
+                    <div className='w-fit flex items-center'>
+                        <FiTrash2 onClick={() => handleDelete(data.id)} className='text-xl hover:cursor-pointer' />
+                        <Checkbox className='p-4 hover:cursor-pointer' />
+                        {data.task}
+                        <BiEditAlt className='cursor-pointer' />
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
